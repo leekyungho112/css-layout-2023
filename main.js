5116f992468baf084655802b5295db5ffe686180
kyungho <epsoe@naver.com>
2023-11-11 15:17:40 +0900

    setting

---
+++ main.js
@@ -0,0 +1,13 @@
+import './style.css';
+import javascriptLogo from './javascript.svg';
+import viteLogo from '/vite.svg';
+import { setupCounter } from './counter.js';
+
+document.querySelector('#app').innerHTML = `
+  <div>
+dddddd
+dkdkdkdkdkdddd
+  </div>
+`;
+
+setupCounter(document.querySelector('#counter'));
